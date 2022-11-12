7a129dd8609518080e5b59cb024a2e96ac3e5d83
nfnfj <60233866+nfnfj@users.noreply.github.com>
2022-11-12 08:20:19 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const express =require('express');
+const app = express();
+app.get("/", (request, response) => {
+  const ping = new Date();
+  ping.setHours(ping.getHours() - 3);
+  console.log(`Ping recebido às ${ping.getUTCHours()}:${ping.getUTCMinutes()}:${ping.getUTCSeconds()}`);
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT); // Recebe solicitações que o deixa online
+
+const Discord = require ("discord.js"); / /livraria Discord.js 
+const client = new Discord.Client();  
